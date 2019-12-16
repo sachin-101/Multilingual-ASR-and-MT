f8c6ab379a7767457cce6e53074eb58eeedcd2a4
sachin-101 <sachinkumar04428@gmail.com>
2019-12-16 23:22:53 +0530

    Removed conversion to wav
    Using torchaudio to generate spectrogram, hence no longer needed to convert the mp3 files to wav

--- Code/Speech_To_Text/cv_process.py
+++ Code/Speech_To_Text/cv_process.py
@@ -1,43 +1,29 @@
 # Process and save Common Voice dataset
-import os
-from pydub import AudioSegment
-import pandas as pd
 import re
+import os
+import string
+import pandas as pd
+from shutil import copyfile
+from pydub import AudioSegment
 
-dataset_dir = os.path.join('..','..','Dataset')
-cv_dir = os.path.join(dataset_dir, 'Common voice')
-chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', \
-         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', \ 
-         'y', 'z', ' ', '_']
+
+chars = ['_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', \
+         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
 tokens = [i for i in range(len(chars))]
 tokenize_dict = {c:t for c,t in zip(chars, tokens)}
 
-
-def create_dirs(lang):
-    lang_dir = os.path.join(cv_dir, lang)
-    train_dir = os.path.join(lang_dir, 'train_data')
-    test_dir = os.path.join(lang_dir, 'test_data')
-    try:
-        os.mkdir(train_dir)
-    except FileExistsError:
-        pass
-    try:
-        os.mkdir(test_dir)
-    except FileExistsError:
-        pass
-    return lang_dir, train_dir, test_dir
 
 
 def process_sent(s):
     s = s.lower()
     s = s.translate(str.maketrans('', '', string.punctuation))
-    return [tokenize_dict[c] for c in s]
+    return ','.join([tokenize_dict[c] for c in s])
+
 
 
 def parse_df(df, lan, lang_dir, save_dir):
     """
         Extracts audio and sentence from df
-        Converts mp3 audio to wav
         Returns new df with columns
             "clip" (indexed names)
             "sentence" (corresponding target sentence)
@@ -49,43 +35,40 @@ def parse_df(df, lan, lang_dir, save_dir):
         try:    # some audio files may not exist
             nums = re.findall(r'\d+', sent)
             if len(nums)==0: # avoiding numbers from target sentences
-                clip_name = f'{lan}_{i}.wav'
-                save_clip_dir = os.path.join(save_dir, clip_name)
-                convert_to_wav(clips_dir, clip, save_clip_dir)
+                clip_name = f'{lan}_{i}.mp3'
+                src = os.path.join(clips_dir, clip)
+                dst = os.path.join(save_dir, clip_name)
+                copyfile(src, dst)
                 data.append((clip_name, process_sent(sent)))
                 i += 1 # update counter
         except FileNotFoundError:
-            pass
+            pass 
         except TypeError:
+            pass
+        except KeyError: # for characters such as ú
             pass
     data_df = pd.DataFrame(data, columns=['clip', 'sentence'])
     return data_df
-        
 
-def convert_to_wav(clips_dir, clip, save_clip_dir):
-    """
-        Converts and saves mp3 to wav
-    """
-    mp3_dir = os.path.join(clips_dir, clip)
-    mp3_file = AudioSegment.from_mp3(mp3_dir)
-    mp3_file.export(save_clip_dir, format='wav')
 
     
 if __name__ == "__main__":
-    lang = "English"
-    lang_dir, train_dir, test_dir = create_dirs(lang)
+    lang_dir = os.path.join('English', 'clips')
+    train_dir = 'train_data'
+    test_dir = 'test_data'
 
     # load dataframes
     train_df = pd.read_csv(os.path.join(lang_dir, 'train.tsv'),  delimiter='\t')
     dev_df = pd.read_csv(os.path.join(lang_dir, 'dev.tsv'),  delimiter='\t')
+    train_df = pd.concat([train_df, dev_df])
     test_df = pd.read_csv(os.path.join(lang_dir, 'test.tsv'),  delimiter='\t')
 
     # Extract train data
+    print('Preparing Train dataset')
     train_data_df = parse_df(train_df, 'eng', lang_dir, train_dir) # extract data from train_df 
-    dev_data_df = parse_df(dev_df, 'eng', lang_dir, train_dir) # extract data from train_df
-    total_train_data = pd.concat([train_data_df, dev_data_df])
-    total_train_data.to_csv(os.path.join(train_dir, 'train_data.csv')) # save
-    
+    train_data_df.to_csv(os.path.join(train_dir, 'train_data.csv'))
+
     # Extract test data
+    print('Preparing test dataset')
     test_data_df = parse_df(test_df, 'eng', lang_dir, test_dir) # extract data from test_df
     test_data_df.to_csv(os.path.join(test_dir, 'test_data.csv')) # save
