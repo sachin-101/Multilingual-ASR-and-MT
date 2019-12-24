1468ac7e4e079dcda2fba0bb8710b4c516669dc1
sachin-101 <sachinkumar04428@gmail.com>
2019-12-25 00:17:03 +0530

    Added drop_last=True and organized training files
    The main.py code threw an error when it was at 99% training. So to correct that, I have added drop_last=True to the dataloader

--- Code/Speech_To_Text/LAS Model/main.py
+++ Code/Speech_To_Text/LAS Model/main.py
@@ -41,7 +41,7 @@ def get_chars(lang, train_df=None):
 if __name__ == "__main__":
     
     
-    dataset_dir = '../../../Dataset/aishell_chinese_dataset/dataset'
+    dataset_dir = '../../../Dataset/data_aishell'
     DEVICE = torch.device('cuda') if torch.cuda.is_available() else 'cpu'
     print('DEVICE :', DEVICE)
     
@@ -55,8 +55,25 @@ if __name__ == "__main__":
     # train_df = pd.DataFrame(data, columns=['id', 'sent'])
     # train_df.to_csv(os.path.join(dataset_dir, 'train_df.csv'), header=None)
     # print(train_df.head())
+
+    # read the transcript
+    # transcript_dir = '../../../Dataset'
+    # with open(os.path.join(transcript_dir, 'aishell_transcript_v0.8.txt')) as f:
+    #     data_list = f.readlines()
+
+    # data = []
+    # for example in data_list:
+    #     id_, sent = str(example.split(' ')[0]), str(' '.join(example.split(' ')[1:-1])) # -1 to remove '\n'
+    #     data.append((id_, sent))
+
+    # print('Num examples:', len(data))
+    # data_df = pd.DataFrame(data, columns=['id', 'sent'])
+    # data_df.to_csv(os.path.join(transcript_dir, 'data_aishell', 'train_df.csv'))
+
+
      
     train_df = pd.read_csv(os.path.join(dataset_dir, 'train_df.csv'), names=['id', 'sent'])
+    train_df = train_df.dropna(how='any')
     print(train_df.head())
     # test_df = pd.read_csv('test_df.csv', names=['id', 'sent'])
     
@@ -70,7 +87,7 @@ if __name__ == "__main__":
    
     tensorboard_dir = os.path.join('tb_summary')
     train_dataset = SpeechDataset(train_df, dataset_dir, sos_token, char_to_token, eos_token)
-    train_loader = AudioDataLoader(pad_token, train_dataset, batch_size=32, shuffle=True)
+    train_loader = AudioDataLoader(pad_token, train_dataset, batch_size=32, shuffle=True, drop_last=True)
 
     #test_dataset = SpeechDataset(test_df, dataset_dir)
     #test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True, collate_fn=collate_fn)
@@ -92,20 +109,22 @@ if __name__ == "__main__":
 
 
     # Let's start training
-    epochs = 10
+    epochs = 20
     # optimizer = optim.ASGD(model.parameters(), lr=0.2)  # lr = 0.2 used in paper
     optimizer = optim.Adadelta(model.parameters())
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.98)
     log_interval = 5
-    print_interval = 10
+    print_interval = 40
 
-    summary_dir = os.path.join(tensorboard_dir, str(datetime.datetime.now()))
-    print("summary dir:", summary_dir)
+    time = str(datetime.datetime.now())
+    save_dir = os.path.join('Trained Models', f'Training_{time}')
+    os.mkdir(save_dir)
+    summary_dir = os.path.join(tensorboard_dir, time)
     writer = SummaryWriter(summary_dir)
 
     for epoch in range(epochs):
         print("\nTeacher forcing ratio:", model.tf_ratio)
         train(model, DEVICE, train_loader, optimizer, epoch, print_interval, writer, log_interval)
         scheduler.step()                                    # Decrease learning rate
-        torch.save(model.state_dict(), f'las_model_small_{epoch}')
+        torch.save(model.state_dict(), os.path.join(save_dir, f'las_model_{epoch}'))
         model.tf_ratio = max(model.tf_ratio - 0.01, 0.8)    # Decrease teacher force ratio                       
