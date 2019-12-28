fabde95188321280abf0dfdc54edc43e6a24e35d
sachin-101 <sachinkumar04428@gmail.com>
2019-12-28 09:56:34 +0530

    Added faster method to calculate cross_entropy loss
    Took nn.cross_entropy loss out of the for loop

--- Code/Speech_To_Text/LAS Model/seq2seq.py
+++ Code/Speech_To_Text/LAS Model/seq2seq.py
@@ -1,20 +1,21 @@
 import random
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
+from listener import Listener
+from attend_and_spell import AttendAndSpell
 
 class Seq2Seq(nn.Module):
-    def __init__(self, encoder, decoder, criterion, tf_ratio, device):
+    def __init__(self, encoder, decoder, tf_ratio, device):
         super(Seq2Seq, self).__init__()
         self.encoder = encoder
         self.decoder = decoder
         self.tf_ratio = tf_ratio
         self.device = device
-        self.loss = criterion
 
     def forward(self, data, target):
         Ty = target.shape[1]
-        
+
         # forward propagte through encoder
         encoder_output = self.encoder(data)
         
@@ -28,14 +29,37 @@ class Seq2Seq(nn.Module):
                        ( torch.zeros((N, self.decoder.hid_sz)).to(self.device), 
                           torch.zeros((N, self.decoder.hid_sz)).to(self.device)  )
             ]
-        context = torch.zeros(N, self.decoder.n_h).to(self.device)
+        context = torch.zeros((N, self.decoder.n_h)).to(self.device)
 
-        output = []
+        output = []        
         for t in range(0, Ty):
             y_true = target[:, t]   
             y_out, hidden_prev, context = self.decoder(y_in, hidden_prev, encoder_output, context)
             output.append(y_out)
-            loss += self.loss(y_out, y_true)
             teacher_force = random.random() < self.tf_ratio
             y_in = y_true if teacher_force else y_out.max(dim=1)[1]
-        return loss, output
+        
+        out = torch.stack(output, dim=1)
+        loss = F.cross_entropy(out.view(N*Ty, -1), target.view(-1))
+        return  loss, out
+
+    
+if __name__ == "__main__":
+
+    hid_sz = 10
+    embed_size = 20
+    vocab_size = 5
+    ip_size = 40
+    embed_dim = 10
+
+    encoder = Listener(ip_size, 20, 3)
+    decoder = AttendAndSpell(embed_dim, hid_sz, encoder.output_size, vocab_size)
+
+    X = torch.rand((32, 64, ip_size))
+    Y = torch.randint(0, vocab_size, (32, 15))
+
+    s2s = Seq2Seq(encoder, decoder, tf_ratio=1, device='cpu')
+    loss, pred_out = s2s(X, Y)
+    print(loss)
+    print(pred_out.shape)
+    
